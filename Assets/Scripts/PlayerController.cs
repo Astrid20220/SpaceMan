efd5fd24c9db86722f96c49ad9ca7fa5baabbdc2
unknown <yadiragonzalez556@gmail.com>
2022-10-01 20:48:51 -0500

    Suelo con Raycast

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class NewBehaviourScript : MonoBehaviour
     public float SaltoForce = 6f;
     Rigidbody2D RigidBody;
 
+    public LayerMask groundMask
+
     void Awake(){
         rigidBody = GetComponent<Rigidbody2D>();
 
@@ -23,14 +25,30 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
             Salto();
 
         }
     }
 
     void Salto(){
+        if(IsTouchingTheGround()){
+
+        }
         rigidBody.AddForce(Vector2.up*SaltoForce, ForceMode2D.Impulse);
+
+    }
+    
+    //Nos indica si el personaje esta o no tovando el suelo
+    bool IsTouchingTheGround(){
+        if(Physics2D.Raycast(this.transform.position, Vector2.down, 1.5f, groundMask)){
+
+            //TODO: programar logica de contacto con el suelo
+            return true;
+        }else{
+            //TODO: programar logica de no contacto 
+            return false;
+        }
 
     }
 }
