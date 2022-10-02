29e9382f4631e71d691e6499bbb1eb3ae825da6e
unknown <yadiragonzalez556@gmail.com>
2022-10-01 20:11:46 -0500

    Script de control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //Variables del movimiento del personaje
+    public float SaltoForce = 6f;
+    Rigidbody2D RigidBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Salto(){
+
+    }
+}
