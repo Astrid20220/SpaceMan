d2a5ba094804f7238fe2d10f144015058ffbb57e
unknown <yadiragonzalez556@gmail.com>
2022-10-01 20:29:49 -0500

    Funciones en C# y unity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class NewBehaviourScript : MonoBehaviour
     //Variables del movimiento del personaje
     public float SaltoForce = 6f;
     Rigidbody2D RigidBody;
+
+    void Awake(){
+        rigidBody = GetComponent<Rigidbody2D>();
+
+
+    }
     
     // Start is called before the first frame update
     void Start()
@@ -17,10 +23,14 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            Salto();
+
+        }
     }
 
     void Salto(){
+        rigidBody.AddForce(Vector2.up*SaltoForce, ForceMode2D.Impulse);
 
     }
 }
