69fd2efc558d4a32179106340c3e102ff5b51ef1
Astrid20220 <yadiragonzalez556@gmail.com>
2022-10-28 23:04:58 -0500

    Iniciar movimiento de la plataforma

---
+++ Assets/Scripts/MovingPlataform.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+
+private void OnTriggerEnter2D collision{
+{
+       Animator animator = GetComponent<Animator>();
+       animator.enabled = true;
+    
+}
+
+ 
+        
+    }
+
