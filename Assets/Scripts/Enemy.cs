a231adb48524f518e935ff5e4c9ce579a2e8a98f
Astrid20220 <yadiragonzalez556@gmail.com>
2022-10-28 23:24:57 -0500

    Enemigos moviles-preparando giro

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -24,8 +24,21 @@ private void Awake(){
         this.transform.position = startPosition;
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        
+       float currentRunningSpeed = rinningSpeed;
+
+       if(facingRigth){
+        currentRunningSpeed = runningSpeed;
+        this.transform.eulerAngles = new Vector3(0, 180, 0);
+       } else{
+        currentRunningSpeed = -runningSpeed;
+        this.transform.eulerAngles = Vector3.zero;
+       }
+
+       if(GameManager.sharedInstance.currentGameState == GameState.inGame){
+        rigidBody.velocity = new Vector2(currentRunningSpeed,
+                                         rigidBody.velocity.y);
+       }
     }
 }
